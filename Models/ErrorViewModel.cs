2c1272dc88cea8fb101edd5c6788ab9bafffd8d2
Juan Higuita <jhiguitac@gmail.com>
2024-02-07 16:05:22 -0500

    Inicio Creación Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace calculador_distancia_euclidiano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
