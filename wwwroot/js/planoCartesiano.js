13285c41cb55bbaedc6bb4ab7bcb596d1cbdc512
Juan Higuita <jhiguitac@gmail.com>
2024-02-29 00:14:24 -0500

    Se finaliza captura de datos y generación de la gráfica

--- wwwroot/js/planoCartesiano.js
+++ wwwroot/js/planoCartesiano.js
@@ -1,129 +1,157 @@
 const contexto = document.querySelector('#planoCartesiano');
 
+let puntoInicialX = 0;
+let puntoInicialY = 0;
+let puntoFinalX = 0;
+let puntoFinalY = 0;
+
 function calcularDistancia(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 }
 
+function capturarDatosGrafica(id) {
+    let fila = document.getElementById('fila-' + id);
+    let puntoInicialX = parseFloat(fila.cells[0].innerText);
+    let puntoInicialY = parseFloat(fila.cells[1].innerText);
+    let puntoFinalX = parseFloat(fila.cells[2].innerText);
+    let puntoFinalY = parseFloat(fila.cells[3].innerText);
 
-var puntoA = { x: 0, y: 4 };
-var puntoB = { x: 4, y: 7 };
-var distanciaAB = calcularDistancia(puntoA.x, puntoA.y, puntoB.x, puntoB.y);
+    console.log("Punto Inicial X: " + puntoInicialX);
+    console.log("Punto Inicial Y: " + puntoInicialY);
+    console.log("Punto Final X: " + puntoFinalX);
+    console.log("Punto Final Y: " + puntoFinalY);
 
-var datos = {
-    labels: ["A", "B"],
-    datasets: [{
-        label: 'Resultado Distancia ' + distanciaAB + ' | ',
-        data: [puntoA, puntoB],
-        backgroundColor: "#B60505",
-        borderColor: "#B60505",
-        borderWidth: 2,
-        pointBorderWidth: 4
-    }]
-};
+    let puntoA = { x: puntoInicialX, y: puntoInicialY };
+    let puntoB = { x: puntoFinalX, y: puntoFinalY };
+    let distanciaAB = calcularDistancia(puntoA.x, puntoA.y, puntoB.x, puntoB.y);
 
-var configuracionesGrafico = {
-    scales: {
-        x: {
-            type: 'linear',
-            position: 'bottom'
-        },
-        y: {
-            min: 0
-        }
-    },
-    pluggins: {
-        titile: {
-            display: true,
-            text: 'Puntos en el Plano Cartesiano'
-        },
-        annotation: {
-            annotations: [
-                {
-                    type: 'line',
-                    mode: 'vertical',
-                    scaleID: 'x',
-                    value: puntoA.x,
-                    borderColor: 'red',
-                    borderWidth: 2,
-                    label: {
-                        content: 'A',
-                        enabled: true
-                    }
-                },
-                {
-                    type: 'line',
-                    mode: 'vertical',
-                    scaleID: 'x',
-                    value: puntoB.x,
-                    borderColor: 'red',
-                    borderColor: 'blue',
-                    borderWidth: 2,
-                    label: {
-                        content: 'B',
-                        enabled: true,
-                    }
-                },
-                {
-                    type: 'line',
-                    mode: 'horizontal',
-                    scaleID: 'y',
-                    value: puntoA.y,
-                    borderColor: 'red',
-                    borderWidth: 2,
-                    label: {
-                        content: 'A',
-                        enabled: true
-                    }
-                },
-                {
-                    type: 'line',
-                    mode: 'horizontal',
-                    scaleID: 'y',
-                    value: puntoB.y,
-                    borderColor: 'red',
-                    borderWidth: 2,
-                    label: {
-                        content: 'B',
-                        enbaled: true
-                    }
-                },
-                {
-                    type: 'line',
-                    mode: 'vertical',
-                    scaleID: 'x',
-                    value: (puntoA.x + puntoB.x) / 2,
-                    borderColor: 'green',
-                    borderWidth: 2,
-                    label: {
-                        content: 'Distancia: ' + distanciaAB.toFixed(2),
-                        enabled: true
-                    }
-                }
-            ]
-        },
-        tooltip: {
-            callbacks: {
-                label: function (context) {
-                    var label = context.datasets.label || '';
+    actualizarGrafica(puntoA, puntoB, distanciaAB);
+}
 
-                    if (context.parsed.x !== null && context.parsed.y !== null) {
-                        label += ' (' + context.parsed.x + ', ' + context.parsed.y + ') ';
-                    }
+function actualizarGrafica(puntoA, puntoB, distanciaAB) {
+    let datos = {
+        labels: ["A", "B"],
+        datasets: [{
+            label: 'Resultado Distancia ' + distanciaAB.toFixed(3) + ' | ',
+            data: [puntoA, puntoB],
+            backgroundColor: "#B60505",
+            borderColor: "#B60505",
+            borderWidth: 2,
+            pointBorderWidth: 4
+        }]
+    };
 
-                    return label;
-                }
+    let configuracionesGrafico = {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                min: 0
             }
         },
-        legend: {
-            labels: {
-                fontSize: 20
+        plugins: {
+            title: {
+                display: true,
+                text: 'Puntos en el Plano Cartesiano'
+            },
+            annotation: {
+                annotations: [
+                    {
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'x',
+                        value: puntoA.x,
+                        borderColor: 'red',
+                        borderWidth: 2,
+                        label: {
+                            content: 'A',
+                            enabled: true
+                        }
+                    },
+                    {
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'x',
+                        value: puntoB.x,
+                        borderColor: 'blue',
+                        borderWidth: 2,
+                        label: {
+                            content: 'B',
+                            enabled: true,
+                        }
+                    },
+                    {
+                        type: 'line',
+                        mode: 'horizontal',
+                        scaleID: 'y',
+                        value: puntoA.y,
+                        borderColor: 'red',
+                        borderWidth: 2,
+                        label: {
+                            content: 'A',
+                            enabled: true
+                        }
+                    },
+                    {
+                        type: 'line',
+                        mode: 'horizontal',
+                        scaleID: 'y',
+                        value: puntoB.y,
+                        borderColor: 'red',
+                        borderWidth: 2,
+                        label: {
+                            content: 'B',
+                            enabled: true
+                        }
+                    },
+                    {
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'x',
+                        value: (puntoA.x + puntoB.x) / 2,
+                        borderColor: 'green',
+                        borderWidth: 2,
+                        label: {
+                            content: 'Distancia: ' + distanciaAB.toFixed(2),
+                            enabled: true
+                        }
+                    }
+                ]
+            },
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        let label = context.dataset.label || '';
+
+                        if (context.parsed.x !== null && context.parsed.y !== null) {
+                            label += ' (' + context.parsed.x + ', ' + context.parsed.y + ') ';
+                        }
+
+                        return label;
+                    }
+                }
+            },
+            legend: {
+                labels: {
+                    fontSize: 20
+                }
             }
         }
+    };
+
+    // Eliminar la gráfica existente antes de crear una nueva
+    if (window.miGrafico) {
+        window.miGrafico.destroy();
     }
-};
 
-var miGrafico = new Chart(contexto, {
-    type: 'line',
-    data: datos,
-    options: configuracionesGrafico
-});
+    window.miGrafico = new Chart(contexto, {
+        type: 'line',
+        data: datos,
+        options: configuracionesGrafico
+    });
+}
+
+// Llamar a la función capturarDatosGrafica con valores predeterminados
+actualizarGrafica({ x: puntoInicialX, y: puntoInicialY }, { x: puntoFinalX, y: puntoFinalY }, calcularDistancia(puntoInicialX, puntoInicialY, puntoFinalX, puntoFinalY));
